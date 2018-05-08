7a00ec428dbd9747f9646c6f2e38f057e4c3ac8a
jtorre20 <jtorres1989@gmail.com>
2018-05-08 16:34:59 -0400

    grid, random sequence

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('#character-list').innerHTML += markup;
     })
   );
-  
 
+
+
+// box13.style.backgroundColor = 'black';
+
+  // let box14 = document.getElementById("")
+  // let box15 = document.getElementById("0-0")
+  // let box16 = document.getElementById("0-0")
+  // let box17 = document.getElementById("0-0")
+  // let box18 = document.getElementById("0-0")
+  // let box19 = document.getElementById("0-0")
+  // let box20 = document.getElementById("0-0")
+  // let box21 = document.getElementById("0-0")
+  // let box22 = document.getElementById("0-0")
+  // let box23 = document.getElementById("0-0")
+  // let box24 = document.getElementById("0-0")
+  // let box25 = document.getElementById("0-0")
 });
