38c75d50f16c3659c1ac445c8c9067104c769d1a
mkenane <morkenan@hotmail.com>
2018-05-09 15:52:04 -0400

    second commit

--- src/round.js
+++ src/round.js
@@ -6,35 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let counter = 0;
 });
 function boxArray() {
-  let box1 = document.getElementById("0-0");
-  let box2 = document.getElementById("0-1");
-  let box3 = document.getElementById("0-2");
-  let box4 = document.getElementById("0-3");
-  let box5 = document.getElementById("0-4");
-  let box6 = document.getElementById("1-0");
-  let box7 = document.getElementById("1-1");
-  let box8 = document.getElementById("1-2");
-  let box9 = document.getElementById("1-3");
-  let box10 = document.getElementById("1-4");
-  let box11 = document.getElementById("2-0");
-  let box12 = document.getElementById("2-1");
-  let box13 = document.getElementById("2-2");
-
-  return [
-    box1,
-    box2,
-    box3,
-    box4,
-    box5,
-    box6,
-    box7,
-    box8,
-    box9,
-    box10,
-    box11,
-    box12,
-    box13
-  ];
+  let boxes = [];
+  for (let row = 0; row < 5; row++) {
+    for (let column = 0; column < 5; column++) {
+      let box = document.getElementById(`${row}-${column}`);
+      boxes.push(box);
+    }
+  }
+  return boxes;
 }
 function getGrid() {
   return document.getElementById("identicon");
@@ -73,15 +52,15 @@ function resetAllWhite(boxarray) {
 
 function listenForSelection() {
   eventArray = [];
-  clickCount = sqarray.length;
+  // clickCount = sqarray.length;
 
   document.getElementById("main").addEventListener("click", e => {
     eventArray.push(e.target);
-    console.log("cool");
+    e.target.style.backgroundColor = "purple";
   });
-  if (eventArray === sqarray) {
-    console.log("coorect");
-  }
+  // if (eventArray === sqarray) {
+  //   console.log("coorect");
+  // }
 }
 
 // generate a new sequence
